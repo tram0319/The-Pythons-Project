3c58a7327d510dbcc6a3b0964d172ed114a301f7
kasey722 <149208368+kasey722@users.noreply.github.com>
2023-11-07 14:12:58 -0600

    Update Customer_Class.py

--- Customer_Class.py
+++ Customer_Class.py
@@ -8,10 +8,7 @@ class Customer:
     self.email = email
     self.currentRentals = currentRentals
     self.rentalHistory = currentRentals
-
-  def getInfo(self):
-    return f"Address: {self.address} / Phone Number: {self.phoneNumber} / Email Address: {self.email}"
-    # This method can be deleted if we need to get address, phoneNumber, and email separately
+    
 
   def addRental(self, Video):
     self.currentRentals.append(Video)
@@ -23,14 +20,35 @@ class Customer:
     except:
       print("Video not found in list")
 
+  
   def getFirstName(self):
     return self.firstName
 
   def getLastName(self):
     return self.lastName
 
+  def getInfo(self):
+    return f"Address: {self.address} / Phone Number: {self.phoneNumber} / Email Address: {self.email}"
+    # This method can be deleted if we need to get address, phoneNumber, and email separately
+  
   def getCurrentRentals(self):
     return self.currentRentals
 
   def getRentalHistory(self):
     return self.rentalHistory
+
+  
+  def editFirstName(self, firstName):
+    self.firstName = firstName
+
+  def editLastName(self, lastName):
+    self.lastName = lastName
+
+  def editAddress(self, address):
+    self.address = address
+
+  def editPhoneNumber(self, phoneNumber):
+    self.phoneNumber = phoneNumber
+
+  def editEmail(self, email):
+    self.email = email
