887442bcf1b4678265b2632c2f16f36a707171bc
tram0319 <150065424+tram0319@users.noreply.github.com>
2023-11-08 15:33:26 -0600

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -8,16 +8,22 @@ root.title('Inventory System for Blockbuster Video')
 notebook = ttk.Notebook(root)
 notebook.pack(pady=10, expand=True)
 
-# create 2 frames
+# create frames
 customer = ttk.Frame(notebook)
 video = ttk.Frame(notebook)
+Rental = ttk.Frame(notebook)    
+returnn = ttk.Frame(notebook)
 
 customer.pack(fill='both', expand=True)
 video.pack(fill='both', expand=True)
+Rental.pack(fill='both', expand=True)
+returnn.pack(fill='both', expand=True)
 
 # add frames to notebook
 notebook.add(customer, text='Customer')
 notebook.add(video, text='Video')
+notebook.add(Rental, text='Rent')
+notebook.add(returnn, text='Return')
 
 #Customer Information label
 tk.Label(customer, text="Customer Information").pack()
