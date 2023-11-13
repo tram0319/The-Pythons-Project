c416640c2bcc851b75381e99c9644d4afdfe974d
kasey722 <149208368+kasey722@users.noreply.github.com>
2023-11-13 16:46:16 -0600

    Update Customer_Class.py

--- Customer_Class.py
+++ Customer_Class.py
@@ -1,53 +1,53 @@
 class Customer:
-
-  def __init__(self, firstName, lastName, address, phoneNumber, email, currentRentals):
-    self.firstName = firstName
-    self.lastName = lastName
-    self.address = address
-    self.phoneNumber = phoneNumber
-    self.email = email
-    self.currentRentals = currentRentals
-    self.rentalHistory = currentRentals
     
+    def __init__(self, firstName, lastName, address, phoneNumber, email, currentRentals):
+        self.firstName = firstName
+        self.lastName = lastName
+        self.address = address
+        self.phoneNumber = phoneNumber
+        self.email = email
+        self.currentRentals = currentRentals
+        self.rentalHistory = currentRentals
+      
+    
+    def addRental(self, Video):
+        self.currentRentals.append(Video)
 
-  def addRental(self, Video):
-    self.currentRentals.append(Video)
-
-  def removeRental(self, Video):
-    try:
-      self.currentRentals.remove(Video)
-    except:
-      print("Video not found in list")
-
-  
-  def getFirstName(self):
-    return self.firstName
-
-  def getLastName(self):
-    return self.lastName
-
-  def getInfo(self):
-    return f"Address: {self.address} / Phone Number: {self.phoneNumber} / Email Address: {self.email}"
-    # This method can be deleted if we need to get address, phoneNumber, and email separately
-  
-  def getCurrentRentals(self):
-    return self.currentRentals
-
-  def getRentalHistory(self):
-    return self.rentalHistory
+    def removeRental(self, Video):
+        try:
+            self.currentRentals.remove(Video)
+        except:
+            print("Video not found in list")
 
   
-  def editFirstName(self, firstName):
-    self.firstName = firstName
+    def getFirstName(self):
+        return self.firstName
 
-  def editLastName(self, lastName):
-    self.lastName = lastName
+    def getLastName(self):
+        return self.lastName
 
-  def editAddress(self, address):
-    self.address = address
+    def getInfo(self):
+        return f"Address: {self.address} / Phone Number: {self.phoneNumber} / Email Address: {self.email}"
+        # This method can be deleted if we need to get address, phoneNumber, and email separately
+  
+    def getCurrentRentals(self):
+        return self.currentRentals
 
-  def editPhoneNumber(self, phoneNumber):
-    self.phoneNumber = phoneNumber
+    def getRentalHistory(self):
+        return self.rentalHistory
 
-  def editEmail(self, email):
-    self.email = email
+  
+    def editFirstName(self, firstName):
+        self.firstName = firstName
+
+    def editLastName(self, lastName):
+        self.lastName = lastName
+
+    def editAddress(self, address):
+        self.address = address
+
+    def editPhoneNumber(self, phoneNumber):
+        self.phoneNumber = phoneNumber
+
+    def editEmail(self, email):
+        self.email = email
