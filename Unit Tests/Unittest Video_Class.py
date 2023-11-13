e761f759b5e84ab651a1329c118e894337949b5c
tram0319 <150065424+tram0319@users.noreply.github.com>
2023-11-13 17:09:33 -0600

    Update Unittest Video_Class.py

--- Unit Tests/Unittest Video_Class.py
+++ Unit Tests/Unittest Video_Class.py
@@ -5,7 +5,7 @@ class TestVideo(unittest.TestCase):
 
     def test_constructor(self):
         video = Video("Kill Bill: Volume 1", 2003, "Quentin Tarantino",
-                      8.2, "Action", "Unrented")
+                      8, "Action", "Available")
         result = video.name
         self.assertEqual(result, 'Kill Bill: Volume 1')
 
@@ -16,88 +16,88 @@ class TestVideo(unittest.TestCase):
         self.assertEqual(result, 'Quentin Tarantino')
 
         result = video.rating
-        self.assertEqual(result, 8.2)
+        self.assertEqual(result, 8)
 
         result = video.genre
         self.assertEqual(result, 'Action')
 
         result = video.rentalStatus
-        self.assertEqual(result, 'Unrented')
+        self.assertEqual(result, 'Available')
 
     def test_getName(self):
         video = Video("Kill Bill: Volume 1", 2003, "Quentin Tarantino",
-                      8.2, "Action", "Unrented")
+                      8, "Action", "Available")
         result = video.getName()
         self.assertEqual(result, 'Kill Bill: Volume 1')
 
     def test_getYear(self):
         video = Video("Kill Bill: Volume 1", 2003, "Quentin Tarantino",
-                      8.2, "Action", "Unrented")
+                      8, "Action", "Available")
         result = video.getYear()
         self.assertEqual(result, 2003)
 
     def test_getDirector(self):
         video = Video("Kill Bill: Volume 1", 2003, "Quentin Tarantino",
-                      8.2, "Action", "Unrented")
+                      8, "Action", "Available")
         result = video.getDirector()
         self.assertEqual(result, 'Quentin Tarantino')
 
     def test_getRating(self):
         video = Video("Kill Bill: Volume 1", 2003, "Quentin Tarantino",
-                      8.2, "Action", "Unrented")
+                      8, "Action", "Available")
         result = video.getRating()
-        self.assertEqual(result, 8.2)
+        self.assertEqual(result, 8)
 
     def test_getGenre(self):
         video = Video("Kill Bill: Volume 1", 2003, "Quentin Tarantino",
-                      8.2, "Action", "Unrented")
+                      8, "Action", "Available")
         result = video.getGenre()
         self.assertEqual(result, 'Action')
 
     def test_getRentalStatus(self):
         video = Video("Kill Bill: Volume 1", 2003, "Quentin Tarantino",
-                      8.2, "Action", "Unrented")
+                      8, "Action", "Available")
         result = video.getRentalStatus()
-        self.assertEqual(result, 'Unrented')
+        self.assertEqual(result, "Available")
 
     def test_editName(self):
        video = Video("Kill Bill: Volume 1", 2003, "Quentin Tarantino",
-                      8.2, "Action", "Unrented")
+                      8, "Action", "Available")
        new_name = "Alien"
        video.editName(new_name)
        self.assertEqual(video.name, "Alien")
 
     def test_editYear(self):
         video = Video("Alien", 2003, "Quentin Tarantino",
-                      8.2, "Action", "Unrented")
+                      8, "Action", "Available")
         new_year = 1979
         video.editYear(new_year)
         self.assertEqual(video.year, 1979)
 
     def test_editDirector(self):
         video = Video("Alien", 1979, "Quentin Tarantino",
-                      8.2, "Action", "Unrented")
+                      8, "Action", "Available")
         new_director = "Ridley Scott"
         video.editDirector(new_director)
         self.assertEqual(video.director, "Ridley Scott")
 
     def test_editRating(self):
         video = Video("Alien", 1979, "Ridley Scott",
-                      8.2, "Action", "Unrented")
-        new_rating = 8.5
+                      8, "Action", "Available")
+        new_rating = 5
         video.editRating(new_rating)
-        self.assertEqual(video.rating, 8.5)
+        self.assertEqual(video.rating, 5)
 
     def test_editGenre(self):
         video = Video("Alien", 1979, "Ridley Scott",
-                      8.5, "Action", "Unrented")
+                      5, "Action", "Available")
         new_genre = "Horror"
         video.editGenre(new_genre)
         self.assertEqual(video.genre, "Horror")
 
     def test_editRentalStatus(self):
         video = Video("Alien", 1979, "Ridley Scott",
-                      8.5, "Horror", "Unrented")
+                      5, "Horror", "Available")
         new_rentalStatus = "Rented"
         video.editRentalStatus(new_rentalStatus)
         self.assertEqual(video.rentalStatus, "Rented")
